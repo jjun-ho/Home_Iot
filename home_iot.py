756c29a0fb30adc5ae90e6f5f8ba1619f494167d
jjun-ho <gkswnsgh2@naver.com>
2022-01-12 01:04:09 +0900

    1

--- home_iot.py
+++ home_iot.py
@@ -3,6 +3,17 @@ from gtts import gTTS  #Google Text To Speech
 import os
 import time
 import playsound #mp3파일 재생
+import pandas as pd
+import openpyxl
+
+data = {
+    '목적': ['불', '온도', '습도', '커튼', '창문'],
+    '동작': ['켜', '꺼', '알려', '열어', '닫아'],
+    '어미': ['줘', 'NaN', 'NaN', 'NaN', 'NaN' ]
+}
+df = pd.DataFrame(data)
+
+case = 0
 
 def speak(text):
     tts = gTTS(text=text, lang='ko')
@@ -17,10 +28,79 @@ def get_audio():
         said =" "
         try:
             said = r.recognize_google(audio, language='ko-KR')
-            print(said)
+            if(said == "자비스"):
+                speak("네")
+                audio = r.listen(source)
+                said =" "
+                said = r.recognize_google(audio, language='ko-KR')
+                print(said)
         except Exception as e:
             print("Exception: " + str(e))
     return said
 
 speak("안녕하세요 자비스입니다")
-get_audio()
+s_text = get_audio()
+s_list = s_text.split()
+print(s_list)
+object = ""
+motion= ""
+ending = ""
+for word in s_list:
+    if "불" in word:
+        object = word
+    elif "온도" in word:
+        object = word
+    elif "습도" in word:
+        object = word
+    elif "커튼" in word:
+        object = word
+    elif "창문" in word:
+        object = word
+
+    elif "켜" in word:
+        motion = word
+    elif "꺼" in word:
+        motion = word
+    elif "알려" in word:
+        motion = word
+    elif "열어" in word:
+        motion = word
+    elif "닫아" in word:
+        motion = word
+
+    elif "줘" in word:
+        ending = word
+
+print("object: " + object)
+print("motion: " + motion)
+print("ending: " + ending)
+
+if (object == "불") & (motion == "켜") & (ending == "줘"):
+    case = 1
+elif (object == "불") & (motion == "꺼") & (ending == "줘"):
+    case = 2
+elif (object == "온도") & (motion == "알려") & (ending == "줘"):
+    case = 3
+elif (object == "습도") & (motion == "알려") & (ending == "줘"):
+    case = 4
+elif (object == "커튼") & (motion == "열어") & (ending == "줘"):
+    case = 5
+elif (object == "커튼") & (motion == "닫아") & (ending == "줘"):
+    case = 6
+elif (object == "창문") & (motion == "열어") & (ending == "줘"):
+    case = 7
+elif (object == "창문") & (motion == "닫아") & (ending == "줘"):
+    case = 8
+
+print("case: " + str(case))
+
+inventors = pd.DataFrame(
+    {
+    '목적': [object],
+    '동작': [motion],
+    '어미': [ending],
+    'case': [case]
+    })
+
+inventors.to_excel('iot.xlsx')
+
